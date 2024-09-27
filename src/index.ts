b1ee2c3d693ddc44aeb3144a627b9caca34d4ece
Vinícius Gutierrez <srgutyerrez@gmail.com>
2024-09-27 10:04:08 -0300

    chore: disable rule @typescript-eslint/no-misused-promises

--- src/index.ts
+++ src/index.ts
@@ -248,7 +248,6 @@ export default [
       '@typescript-eslint/no-implied-eval': 'error',
       '@typescript-eslint/no-loss-of-precision': 'error',
       '@typescript-eslint/no-misused-new': 'error',
-      '@typescript-eslint/no-misused-promises': 'error',
       '@typescript-eslint/no-namespace': 'error',
       '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
       '@typescript-eslint/no-redundant-type-constituents': 'error',
