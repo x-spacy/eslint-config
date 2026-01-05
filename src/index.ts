a2afff79cdf5350e50ccfae136ad0fa65dc63358
Vinícius Gutierrez <srgutyerrez@gmail.com>
2026-01-05 01:24:59 -0300

    Fix syntax error in index.ts

--- src/index.ts
+++ src/index.ts
@@ -224,7 +224,7 @@ export default [
             '/^class-validator/',
             [
               '/^axios/'
-            ]'
+            ],
             [
               'module'
             ],
